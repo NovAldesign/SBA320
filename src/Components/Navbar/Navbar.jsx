97d4311812dcd1105bfd3d1b5c0c775fc65adad1
NovAldesign <williams.lavon@gmail.com>
2026-03-01 08:02:58 -0500

    updated css for index and navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import logo from '../../assets/gfcLogo.png'
 const Navbar = () => {
   return (
     <nav>
-        <img src={logo} alt="Grown Folks Collective logo, navy blue and gold, with white background." />
-        <ul>
+        <img src={logo} class="nav-logo" alt="Grown Folks Collective logo, navy blue and gold, with white background." />
+        <ul class="nav-links">
         <li>Home</li>
         <li>Events</li>
         </ul>
